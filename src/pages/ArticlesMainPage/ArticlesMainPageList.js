ecfecb93227e5f61781f61c620ee4a4d0f26625d
tabatintores <big_clifty@list.ru>
2020-01-01 17:00:58 +0500

    Разработка приложения

--- src/pages/ArticlesMainPage/ArticlesMainPageList.js
+++ src/pages/ArticlesMainPage/ArticlesMainPageList.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import {articlesStorage} from "../../modules/ArticlesVariables";
 import {saveArticlesStorage} from "../../modules/ArticlesMethods";
+import {ArticlesMainPageListItem} from "./ArticlesMainPageListItem";
+import {MDBBtn} from "mdbreact";
 
 export const ArticlesMainPageList = () => {
-    const testArticle = {
-        id: 1,
-        title: 'Как приручить дракона?',
-        description: 'Берёшь и приручаешь...',
-        text: 'Я всё сказал.',
-        comments: [],
-        date: new Date().toLocaleDateString()
-    }
-    saveArticlesStorage(testArticle);
+
     return (
-        !articlesStorage.length === 0 ?
-            <h1>Статьи!</h1>
+        articlesStorage.length == 0 ?
+            <>
+                <MDBBtn color="primary" outline rounded>Primary</MDBBtn>
+                <MDBBtn rounded outline>Default</MDBBtn>
+                <MDBBtn rounded outline color="secondary">Secondary</MDBBtn>
+                <MDBBtn rounded outline color="success">Success</MDBBtn>
+                <MDBBtn rounded outline color="info">Info</MDBBtn>
+                <MDBBtn rounded outline color="warning">Warning</MDBBtn>
+                <MDBBtn rounded outline color="danger">Danger</MDBBtn>
+            </>
+
+            /*articlesStorage.map(article => {
+                const {id,title,description,text,comments} = article;
+                return (
+                    <ArticlesMainPageListItem
+                        key={id}
+                        title={title}
+                        description={description}
+                        text={text}
+                        comments={comments}
+                    />
+                )*/
+            // })
          : <h2>Статей нет!</h2>
     )
 };
